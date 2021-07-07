c8105feb4caee0152adb7482e7ac9709b915ff85
Majed Alfiqi <majidalfiqi@gmail.com>
2021-07-07 03:24:25 +0100

    Finished the markup and the styling using tailwindcss classes

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [["./**/*.html", "./**/*.js"]],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
